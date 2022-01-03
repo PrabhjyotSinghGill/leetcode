23088865169d41820acd546278fc7e84fdd9e869
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2022-01-03 11:17:42 -0500

    Medium -- Remove Nth Node From End of List

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 class Solution:
-    def reverse(self, x: int) -> int:
-        abs_value = abs(x)
-        sign = 1 if x >= 0 else -1 
-        reverse = 0
-        max_value = 2 ** 31 - 1
-        while abs_value > 0:
-            reverse = reverse * 10 + (abs_value % 10)
-            abs_value //= 10
-            if reverse > max_value:
-                return 0
-        return reverse * sign
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        slowPtr = head
+        fastPtr = head
+
+        while n >= 0 and fastPtr:
+            fastPtr = fastPtr.next
+            n -= 1
+        if n > 0:
+            return head
+        elif n == 0:
+            return head.next
+        while fastPtr:
+            fastPtr = fastPtr.next
+            slowPtr = slowPtr.next
+        if slowPtr and slowPtr.next:
+            slowPtr.next = slowPtr.next.next
+        
+        return head

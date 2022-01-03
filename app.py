a0d63e1bff3dc55415248d4aef8767b606d8e1c5
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2022-01-03 16:06:21 -0500

    Medium -- Pow(x,n)

--- app.py
+++ app.py
@@ -1,19 +1,3 @@
 class Solution:
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        slowPtr = head
-        fastPtr = head
-
-        while n >= 0 and fastPtr:
-            fastPtr = fastPtr.next
-            n -= 1
-        if n > 0:
-            return head
-        elif n == 0:
-            return head.next
-        while fastPtr:
-            fastPtr = fastPtr.next
-            slowPtr = slowPtr.next
-        if slowPtr and slowPtr.next:
-            slowPtr.next = slowPtr.next.next
-        
-        return head
+    def myPow(self, x: float, n: int) -> float:
+        return x**n

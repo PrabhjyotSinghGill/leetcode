4a7af8e7dfea9b4b7e259081b2816efa972f6e0f
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-01-05 15:32:47 -0500

    Pascal's Triangle.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#solution
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         dp = [[1]*(i+1) for i in range(numRows)]
@@ -5,3 +6,4 @@ class Solution:
             for j in range(1,i):
                 dp[i][j] = dp[i-1][j]+dp[i-1][j-1]
         return dp
+        
